8f62b243a71e942cd9811feebcee59a61537a0ef
Oliver Stingl <o.stingl@protonmail.com>
2019-06-25 11:53:22 +0200

    Fixes defindes to defines.

--- webshop_kunde.php
+++ webshop_kunde.php
@@ -2,14 +2,14 @@
 // defines if the page is restricted to logged-in Users only
 $pageRestricted = false;
 
-// defindes the minimum userRole to access the page, if the
+// defines the minimum userRole to access the page, if the
 // userRole is lower than the level, a 403 Error-Page is returned
 $userLevel = 1;
 
 // includes base function like session handling
 include "include/init.php";
 
-// defindes the name of the current page, displayed in the title and as a header on the page
+// defines the name of the current page, displayed in the title and as a header on the page
 $title = "Webshop - Kunde";
 include "include/page/top.php";
 ?>
