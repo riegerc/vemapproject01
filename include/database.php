f8b06ca71b4e9c979740e04accfa95d469bac98b
riegerc <ch_ri@gmx.at>
2019-06-25 13:56:40 +0200

    add writeDB function, not tested yet

--- include/database.php
+++ include/database.php
@@ -23,11 +23,14 @@ function connectDB(): PDO
 
 /**
  * important: the SQL Statement in the $sql variable MUST start with select (case insensitive)!
+ * please use $param array if the statement contains user imput. This goes like this:
+ * $sql = "SELECT * FROM tablename WHERE column1 = :placeholder1 and column2 = :placeholder2;";
+ * $param = array(":placeholder1"=>$variable1,":placeholder2"=>$variable2); This replaces the bindParam()
  * 
  */
-function readDB(string $sql, array $paramy = NULL){
+function readDB(string $sql, array $params = NULL){
     $result = [];
-    if(!isSqlSelect($ql)){return "Kein SELECT SQL Statement!";}
+    if(!isSqlSelect($ql)){return "No SELECT SQL Statement!";}
 
     $db = connectDB();
     
@@ -36,8 +39,31 @@ function readDB(string $sql, array $paramy = NULL){
         $stmt->execute($param);
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }catch(Exception $ex){
-        die("DB - Error");
+        die("DB - Error SELECT");
         return $result;
     }
+
     return $result;
+}
+
+/**
+ * important the SQL statement MUST start with INSERT (case insensitive)!
+ * the param array is to use like in the readDB (only  with an INSERT INTO Statement):
+ * $param = array(":placeholder1"=>$variable1,":placeholder2"=>$variable2); This replaces the bindParam()
+ */
+function writeDB(string $sql, array $params):int{
+    $ret = 0;
+    if(!isSqlInsert($sql)){return -1;}
+
+    $db = connectDB();
+
+    try{
+        $stmt = $db->prepare($sql);
+        $stmt->execute($param);
+        $ret = $db->lastInsertId();
+    }catch(Exception $ex){
+        die("DB - Error INSERT");
+        return -1;
+    }
+    return $ret;
 }
