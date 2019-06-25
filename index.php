8b92a36b48a72ec1e81964eed3fb04dd7300ca20
Oliver Stingl <o.stingl@protonmail.com>
2019-06-25 09:14:53 +0200

    Adds text for landing-page.

--- index.php
+++ index.php
@@ -2,52 +2,41 @@
 // defines if the page is restricted to logged-in Users only
 $pageRestricted = false;
 
-// defindes the minimum userRole to access the page, if the
+// defines the minimum userRole to access the page, if the
 // userRole is lower than the level, a 403 Error-Page is returned
-$userLevel = 1;
+$userLevel = 0;
 
 // includes base function like session handling
 include "snippets/init.php";
 
-// defindes the name of the current page, displayed in the title and as a header on the page
-$title = "Home";
+// defines the name of the current page, displayed in the title and as a header on the page
+$title = "Willkommen am Beschaffungsportal vom AMS Wien";
 include "snippets/top.php";
 ?>
 
-    <div class="container-fluid">
-        <h1 class="h3 mb-4 text-gray-800"><?php echo $title ?></h1>
-        <div class="content">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-            consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-            sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-            elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet.<br/>
-            <br/>
-            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu
-            feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
-            delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing
-            elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.<br/>
-            <br/>
-            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
-            commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
-            vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit
-            praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.<br/>
-            <br/>
-            Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer
-            possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
-            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
-            tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.<br/>
-            <br/>
-            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu
-            feugiat nulla facilisis.<br/>
-            <br/>
-            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
-            est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-        </div>
+<div class="container-fluid">
+    <h1 class="h3 mb-4 text-gray-800"><?php echo $title ?></h1>
+    <div class="content">
+        <p>
+            Dieses B2B Portal ist das zentrale Kommunikationsmedium zu unseren Anbietern und<br>
+            Auftragnehmern, über das wir Einkaufsprojekte abwickeln und damit Prozesse optimieren.<br>
+            <br>
+        </p>
+        <ul>
+            <li>Von uns eingeladene Anbieter können sich hier Ausschreibungsdokumente herunterladen und Angebote
+                abgeben.
+            </li>
+            <li>Mitarbeiter können Standardartikel über den Multilieferantenkatalog abrufen.</li>
+            <li>Um die Zusammenarbeit ständig zu verbessern, werden die Auftragnehmer einer regelmäßigen Bewertung
+                unterzogen.
+            </li>
+
+        </ul>
+        <p>
+            <br>
+            Es werden ausschließlich Technologien verwendet, die einen sicheren Datenaustausch ermöglichen.
+        </p>
     </div>
+</div>
 
 <?php include "snippets/bottom.php"; ?>
