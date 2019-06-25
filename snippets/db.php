e937ecc6220aa6f710e17ea4fac714007135664e
Oliver Stingl <o.stingl@protonmail.com>
2019-06-25 09:02:29 +0200

    Adds new function-based db include-file.

--- snippets/db.php
+++ snippets/db.php
@@ -1,13 +1,21 @@
 <?php
-$dbHost = "remotemysql.com";
-$dbName = "e5En6pGWNA";
-$dbCharset = "utf8";
-$dbUser = "e5En6pGWNA";
-$dbPw = "gwWmxvxZbZ";
 
-try {
-    $db = new PDO("mysql:host=$dbHost;dbname=$dbName;dbcharset=$dbCharset;", "$dbUser", "$dbPw");
-    //echo "Database Connected Successfully <br><br>";
-} catch (PDOException $e) {
-    die("That didn't work");
+function connectDB(): PDO
+{
+    $dbHost = "remotemysql.com";
+    $dbName = "e5En6pGWNA";
+    $dbCharset = "utf8";
+    $dbUser = "e5En6pGWNA";
+    $dbPw = "gwWmxvxZbZ";
+
+    try {
+        return new PDO(
+            "mysql:host=$dbHost;dbname=$dbName;dbcharset=$dbCharset;",
+            "$dbUser",
+            "$dbPw",
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+    } catch (PDOException $e) {
+        die("That didn't work");
+    }
 }
