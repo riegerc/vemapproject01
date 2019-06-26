a812d99df4bf4007fe0e904d5127dd010d19925d
skyiee <skyiee@voemail.com>
2019-06-26 16:20:41 +0200

    ..

--- webshop_lieferant.php
+++ webshop_lieferant.php
@@ -3,6 +3,7 @@ $pageRestricted = false; // defines if the page is restricted to logged-in Users
 $userLevel = 1; // defines the minimum userRole to access the page, if the userRole is lower than the level, a 403 Error-Page is returned
 $title = ""; // defines the name of the current page, displayed in the title and as a header on the page
 
+require_once("include/database.php");
 include "include/init.php"; // includes base function like session handling
 include "include/page/top.php"; // top-part of html-template (stylesheets, navigation, ..)
 ?>
