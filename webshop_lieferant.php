839f3250e61a6b1e17348804853894696c483d39
skyiee <skyiee@voemail.com>
2019-06-27 10:12:07 +0200

    asd

--- webshop_lieferant.php
+++ webshop_lieferant.php
@@ -52,13 +52,13 @@ foreach($db->query($sql) as $row){
     if($row['ordered']==1){
     echo "    <td><br>Bestätigt<br></td>\n";   
     }else{
-    echo "    <td> <a href='#' name='order'>Bestätigen</a><br></td>\n";
+    echo "    <td> <a href='#' name='order' >Bestätigen</a><br></td>\n";
     }
     
     echo "    </tr>";
 }
 
-if($_POST['order']){
+if(isset($_POST['order'])){
     $sql="INSERT ordered INTO orderitems SET ordered='1'";
     $sql->execute();
 }
