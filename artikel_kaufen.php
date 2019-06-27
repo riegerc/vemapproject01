68e9f4fb15e82e8be90624e4c065758bf8f33dab
skyiee <skyiee@voemail.com>
2019-06-27 14:37:18 +0200

    kaufen button fix

--- artikel_kaufen.php
+++ artikel_kaufen.php
@@ -1,49 +1,36 @@
 <?php
-$checkme = "a30ee472364c50735ad1d43cc09be0a1";
-require_once "include/constant.php";
-
 $pageRestricted = false; // defines if the page is restricted to logged-in Users only
-$userLevel = ""; // uses a PERM_ const now and hasPermission($userLevel) now if fails a 403 Error-Page is returned
-$title = "Artikel kaufen"; // defines the name of the current page, displayed in the title and as a header on the page
+$userLevel = 1; // defines the minimum userRole to access the page, if the userRole is lower than the level, a 403 Error-Page is returned
+$title = "Artikel Kaufen"; // defines the name of the current page, displayed in the title and as a header on the page
 
 include "include/init.php"; // includes base function like session handling
 include "include/page/top.php"; // top-part of html-template (stylesheets, navigation, ..)
 ?>
 <?php
-$sql="
-/*SELECT name FROM article
+$sql="SELECT name FROM article
 INNER JOIN orderitems 
-ON article.objectID = orderitems.articleFID*/
-
-SELECT name, price
+ON article.objectID = orderitems.articleFID
+";
+/*SELECT *
 FROM article
 INNER JOIN orderitems
-ON article.objectID = orderitems.articleFID
+ON `article`.objectID = `orderitems`.articleFID
 INNER JOIN order
-ON order.objectID = orderitems.orderFID
-";
-
+ON `order`.objectID = `orderitems`.orderFID*/
 $statement=connectDB()->query($sql);
 $statement->execute();
-while($row=$statement->fetch()){
-    echo $row["price"];
-    $articlePrice=$row["price"];
-}
 
-$amount = $_POST["amount"];
-$article = $_POST["update"];
-$price=$amount*$articlePrice;
+
 $sql="INSERT INTO orderitems
-    (count, articleFID, price)
-    VALUES
-    (:count, :articleFID, :price)";
+   (count, articleFID)
+   VALUES
+   (:count, :articleFID)";
     $statement=connectDB()->prepare($sql);
     $statement->bindParam(":count", $_POST["amount"]);
     $statement->bindParam(":articleFID", $_POST["update"]);
-
     $statement->execute();
-
-
+    $amount = $_POST["amount"];
+    $article = $_POST["update"];
 
 ?>
 <div class="container-fluid">
