529f564506bb1e9ca99480e60f173d6660b21463
Oliver Stingl <o.stingl@protonmail.com>
2019-06-27 09:40:45 +0200

    Introduce dataTables.

--- include/page/bottom.php
+++ include/page/bottom.php
@@ -14,7 +14,14 @@
 <?php include "logoutModal.php"; ?>
 <script src="vendor/jquery/jquery.min.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 <script src="js/sb-admin-2.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#dataTable').DataTable();
+    });
+</script>
 </body>
 </html>
