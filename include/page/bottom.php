ecfababd9de3530b69c1ba57f126f44aafc39228
Oliver Stingl <o.stingl@protonmail.com>
2019-06-28 14:29:57 +0200

    Introduces Vue.js

--- include/page/bottom.php
+++ include/page/bottom.php
@@ -17,6 +17,7 @@
 <script src="vendor/datatables/jquery.dataTables.min.js"></script>
 <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="vendor/vue/vue.js"></script> <!-- Vue.js Dev-Edition; vue.min.js for production -->
 <script src="js/sb-admin-2.min.js"></script>
 <script>
     jQuery(document).ready(function() {
