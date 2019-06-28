f190dc2eee2ef41331b0d58b842cde352adace50
Oliver Stingl <o.stingl@protonmail.com>
2019-06-28 13:19:13 +0200

    Removes nav points from ratings

--- include/page/nav.php
+++ include/page/nav.php
@@ -24,10 +24,7 @@ $navigationItems =
                 ],
                 ["name" => "Bewertungen",
                     "links" => [
-                        ["name" => "Einkauf", "url" => "#", "minUserLevel" => PERM_MAKE_REVIEW],
                         ["name" => "Lieferant", "url" => "#", "minUserLevel" => PERM_VIEW_REVIEW],
-                        ["name" => "", "url" => "kriterien.php", "minUserLevel" => PERM_CED_REVIEW],
-                        ["name" => "", "url" => "fragebogen.php", "minUserLevel" => PERM_MAKE_REVIEW]
                     ],
                     "icon" => "<i class='fas fa-edit'></i>",
                     "minUserLevel" => PERM_VIEW_RATING_MENU
@@ -41,7 +38,6 @@ $navigationItems =
                     "icon" => "<i class='fas fa-users'></i>",
                     "minUserLevel" => PERM_VIEW_CLIENT_MENU
                 ],
-
             ],
             "minUserLevel" => PERM_EDIT_SELF
         ],
