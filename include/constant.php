6aba969f30b2bc85fa651fdec6c8a913045a22ba
skyiee <skyiee@voemail.com>
2019-06-28 11:33:02 +0200

    permission comment clarification

--- include/constant.php
+++ include/constant.php
@@ -27,20 +27,20 @@ const PWD_REQ_LOWER_CASE = 0;
 const PWD_REQ_SPECIAL_CHAR = 0;
 
 #permissions in use ------------------------------------------
-const PERM_EDIT_PERM = "editPermission";
-const PERM_VIEW_OFFER_MENU = "viewOfferMenu";
-const PERM_VIEW_RATING_MENU = "viewRatingMenu";
-const PERM_VIEW_WEBSHOP_MENU  = "viewWebShopMenu";
-const PERM_VIEW_CLIENT_MENU = "viewClientMenu";
-const PERM_CED_SUPPLIER = "cedSupplier";
-const PERM_VIEW_OFFER = "viewOffer";
-const PERM_ORDER_FROM_CATALOGUE = "orderFromCataloge";
-const PERM_INSERT_INTO_CATALOGUE = "insertIntoCataloge";
-const PERM_VIEW_REVIEW = "viewReview";
-const PERM_MAKE_REVIEW = "makeReview";
-const PERM_CED_REVIEW = "cedReview";
-const PERM_CED_USER = "cedUser"; # NOT including SELF!
-const PERM_EDIT_SELF = "editUser"; #everyone can edit him/herself, but noone can delete him/herself
+const PERM_EDIT_PERM = "editPermission"; //external admin only, this permission grants permission to edit PERMISSIONS (rolerights.php)
+const PERM_VIEW_OFFER_MENU = "viewOfferMenu"; // user can see the whole of "Ausschreibungen" category (nav)
+const PERM_VIEW_RATING_MENU = "viewRatingMenu";// user can see the whole of "Bewertungen" category(nav)
+const PERM_VIEW_WEBSHOP_MENU  = "viewWebShopMenu";// user can see the whole of "Webshop" category(nav)
+const PERM_VIEW_CLIENT_MENU = "viewClientMenu";// user can see the whole of "User" category (nav)
+const PERM_CED_SUPPLIER = "cedSupplier";// user can CREATE new supplies(lieferanten), Ausschreibungen->Lieferant anlegen
+const PERM_VIEW_OFFER = "viewOffer"; // user can see Ausschreibungen->"Ihre Ausschreibungen"
+const PERM_ORDER_FROM_CATALOGUE = "orderFromCataloge";//user has access to the Webshop->kunde catalogue
+const PERM_INSERT_INTO_CATALOGUE = "insertIntoCataloge";//user(mainly SUPPLIER) can accept orders
+const PERM_VIEW_REVIEW = "viewReview";//user(mainly SUPPLIER) can see their own RATING(review,feedback,Bewertung)
+const PERM_MAKE_REVIEW = "makeReview";//user can GIVE RATINGS
+const PERM_CED_REVIEW = "cedReview";//internal/external Admin may edit the rating FORM itself under Bewertungen->kriterien
+const PERM_CED_USER = "cedUser"; //internal/external Admin can CREATE users under User->Erstellen
+const PERM_EDIT_SELF = "editUser"; //everyone can edit THEIR OWN DETAILS
 
 #permissions not in use now----------------------------------
 const PERM_VIEW_USER = "viewUser";
