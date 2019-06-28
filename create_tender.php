3f469e494d0fb109e0aa35fff1d8e2603a6b94cb
adnan <mesheel1990@gmail.com>
2019-06-28 08:45:37 +0200

    fiexed post

--- create_tender.php
+++ create_tender.php
@@ -73,6 +73,32 @@ if( isset($_POST["absenden"]) ){
                 (?,?,?,?,?,?,?,?)";
              $stmt=connectDB()->prepare($sql);
              $stmt->execute(array($tender,$tenderType, "30000000-9" ,$begin,$end,$description,$userFID, $amount));
+             #$id = stmt->lastInsertId();
+             #-------------------------------------------------------
+
+
+
+                $sql="SELECT * FROM tenders
+                WHERE objectID = (
+                SELECT MAX(objectID) FROM tenders)";
+                $stmt=connectDB()->query($sql);
+                $currentID=$stmt->fetch();
+                echo $currentID["objectID"];
+
+                foreach($_POST["objectID"] as $key => $value){
+                    $userFID=(int)$_POST["objectID"][$key];
+                    $sql="INSERT INTO supplierselect 
+                    (tenderFID,userFID)
+                    VALUES
+                    (:currentID,:userFID)";
+                    $stmt=connectDB()->prepare($sql);
+                    $stmt->bindParam(":currentID",$currentID);
+                    $stmt->bindParam(":userFID",$userFID);
+                    $stmt->execute();
+
+                }
+
+
 
 
 
@@ -80,7 +106,91 @@ if( isset($_POST["absenden"]) ){
 
 
 
+<?php
+$conn= connectDB();
+$showRecordPerPage = 5;
+if(isset($_GET['page']) && !empty($_GET['page'])){
+    $currentPage = $_GET['page'];
+}else{
+    $currentPage = 1;
+}
+$startFrom = ($currentPage * $showRecordPerPage) - $showRecordPerPage;
+$showRecordPerPage = 5;
+if(isset($_GET['page']) && !empty($_GET['page'])){
+    $currentPage = $_GET['page'];
+}else{
+    $currentPage = 1;
+}
+$startFrom = ($currentPage * $showRecordPerPage) - $showRecordPerPage;
+$totalEmpSQL = "SELECT count(*) FROM user LIMIT $startFrom, $showRecordPerPage";
+#echo $totalEmpSQL;
+$stmt = $conn->query($totalEmpSQL);
+$allEmpResult = $stmt->fetch();
+$totalEmployee = $allEmpResult[0];
+$lastPage = ceil($totalEmployee/$showRecordPerPage);
+$firstPage = 1;
+$nextPage = $currentPage + 1;
+$previousPage = $currentPage - 1;
+#"SELECT user.objectID, user.firstName, user.lastName FROM user LIMIT $startFrom, $showRecordPerPage";
+$empSQL = "SELECT objectID, firstName, lastName , branchName , rolesFID 
+FROM `user`
+WHERE rolesFID=1
+ LIMIT $startFrom, $showRecordPerPage";
+$stmt = $conn->query($empSQL);
+#$empResult = $stmt->fetch();
+?>
+<table class="table ">
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th></th>
+        <th>branchName</th>
+        <th>firstName</th>
+        <th>lastName</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    # var_dump($allEmpResult);
+    while($emp = $stmt->fetch()){
+        ?>
+        <tr>
+            <th scope="row"><?php echo $emp['objectID']; ?></th>
+            <?php
+            echo "<th> <input type='checkbox' name='role' value='$emp[rolesFID]'></th>";
+            echo "<input type='hidden' name='objectID[]' value='$emp[objectID]'>\n";
+            ?>
 
+            <td><?php echo $emp['branchName']; ?></td>
+            <td><?php echo $emp['firstName']; ?></td>
+            <td><?php echo $emp['lastName']; ?></td>
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
+<nav aria-label="Page navigation">
+    <ul class="pagination">
+        <?php if($currentPage != $firstPage) { ?>
+            <li class="page-item">
+                <a class="page-link" href="?page=<?php echo $firstPage ?>" tabindex="-1" aria-label="Previous">
+                    <span aria-hidden="true">First</span>
+                </a>
+            </li>
+        <?php } ?>
+        <?php if($currentPage >= 2) { ?>
+            <li class="page-item"><a class="page-link" href="?page=<?php echo $previousPage ?>"><?php echo $previousPage ?></a></li>
+        <?php } ?>
+        <li class="page-item active"><a class="page-link" href="?page=<?php echo $currentPage ?>"><?php echo $currentPage ?></a></li>
+        <?php if($currentPage != $lastPage) { ?>
+            <li class="page-item"><a class="page-link" href="?page=<?php echo $nextPage ?>"><?php echo $nextPage ?></a></li>
+            <li class="page-item">
+                <a class="page-link" href="?page=<?php echo $lastPage ?>" aria-label="Next">
+                    <span aria-hidden="true">Last</span>
+                </a>
+            </li>
+        <?php } ?>
+    </ul>
+</nav>
 <br>
 
 
