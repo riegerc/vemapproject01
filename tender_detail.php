6b1370078e01917ccfc893a81b3fb5bce63fbc8b
manta-bracket <s@s.s>
2019-07-01 12:21:16 +0200

    fake uploads hinzugefügt

--- tender_detail.php
+++ tender_detail.php
@@ -47,9 +47,9 @@ $dateEnd=date_create($row["end"]);
         <a href="overview_tenders.php" >&#8636 Zurück zu ihren Ausschreibungen</a>
         <h2><?php echo $row["tender"] ?></h2>
         <a href="pdf.php?id=<?php echo $tenderGetID ?>" class="float-right" ><button type="button" class="btn btn-danger"><i class="fas fa-file-pdf"></i>Ausschreibung als PDF herunterladen</button> </a>
-        <a href="/uploads/anhang03_<?php echo $row["tender"] ?>.pdf" class="float-right" ><button type="button" class="btn btn-default"><i class="fas fa-file-download"></i>Anhang 3 herunterladen</button> </a>
-        <a href="/uploads/anhang02_<?php echo $row["tender"] ?>.pdf" class="float-right" ><button type="button" class="btn btn-default"><i class="fas fa-file-download"></i>Anhang 2 herunterladen</button> </a>
-        <a href="/uploads/anhang01_<?php echo $row["tender"] ?>.pdf" class="float-right" ><button type="button" class="btn btn-default"><i class="fas fa-file-download"></i>Anhang 1 herunterladen</button> </a>
+        <a href="/uploads/anhang03_<?php echo $row["tender"] ?>.pdf" class="float-right" ><button type="button" class="btn btn-default"><i class="fas fa-file-download"></i>Anhang 3</button> </a>
+        <a href="/uploads/anhang02_<?php echo $row["tender"] ?>.pdf" class="float-right" ><button type="button" class="btn btn-default"><i class="fas fa-file-download"></i>Anhang 2</button> </a>
+        <a href="/uploads/anhang01_<?php echo $row["tender"] ?>.pdf" class="float-right" ><button type="button" class="btn btn-default"><i class="fas fa-file-download"></i>Anhang 1</button> </a>
         <br>
         <table class="table">
             <thead class="thead-dark">
