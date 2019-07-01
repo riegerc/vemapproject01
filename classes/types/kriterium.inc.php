46fe8a7f14ce65a4dd2b208d92df7d16f2a7790b
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-07-01 14:22:48 +0200

    Fragebgen looks good, 1TODO

--- classes/types/kriterium.inc.php
+++ classes/types/kriterium.inc.php
@@ -36,7 +36,7 @@ class Kriterium{
 	public function __toString(){
 		
 			// begin outer form row
-			$res="<div class='row'>";
+			$res="<div class='form-row'>";
 			
 			$res.="<li class='list-group-item'>$this->name\n";			
 
@@ -46,14 +46,14 @@ class Kriterium{
 			$res.="<div class='form-group'>\n";
 
 			// begin inner form row
-			$res.="<div class='row'>";
+			$res.="<div class='form-row'>";
 			
 			// begin inner form col
 			$res.="<div class='col-1'>";
 			$res.="<span>0</span>\n";
 			$res.="</div>";
 			
-			$res.="<div class='col-9'>";
+			$res.="<div class='col-19'>";
 			
 			$res.="<input type='range' class='custom-range' name='sld$this->id' id='sld{$this->id}' min='0' max='".$this->getPrzt()."' step='0.001' 
 			value='0' onchange='setLabelText(".$this->getId().",".$this->getFkKriterium().")'>";			
@@ -71,12 +71,12 @@ class Kriterium{
 			// end inner form row
 			$res.="</div>";
 			
+			// end outer form row
+			$res.="</div>";
+			
 			// moved the label outside list group
 			$res.="<div class='col-1'>";
 			$res.="<label class='badge badge-secondary' id='lbl".$this->getFkKriterium()."' for='lbl".$this->getFkKriterium()."'></label>\n";
-			$res.="</div>";
-			
-			// end outer form row
 			$res.="</div>";
 			
 			$res.="</li>\n";
