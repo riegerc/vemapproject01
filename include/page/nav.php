9bf82e18d6ba034bce5fb10d6349601736b7d586
Oliver Stingl <o.stingl@protonmail.com>
2019-07-02 12:51:34 +0200

    Shorted overview-nav-item for ratings

--- include/page/nav.php
+++ include/page/nav.php
@@ -24,10 +24,10 @@ $navigationItems =
                 ],
                 ["name" => "Bewertungen",
                     "links" => [
+                        ["name" => "Übersicht", "url" => "review_uebersichts_tabelle.php", "minUserLevel" => PERM_CED_REVIEW],
                         ["name" => "Bewertung verwalten", "url" => "kriterien.php", "minUserLevel" => PERM_CED_REVIEW],
                         ["name" => "Lieferant bewerten", "url" => "lieferant.php?what=1", "minUserLevel" => PERM_MAKE_REVIEW],
                         ["name" => "Auswertung", "url" => "lieferant.php?what=2", "minUserLevel" => PERM_VIEW_REVIEW],
-                        ["name" => "Review Übersichts-Tabelle", "url" => "review_uebersichts_tabelle.php", "minUserLevel" => PERM_CED_REVIEW],
                     ],
                     "icon" => "<i class='fas fa-edit'></i>",
                     "minUserLevel" => PERM_VIEW_RATING_MENU
