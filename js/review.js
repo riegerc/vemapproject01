363e73e32fa0c2e5f53d6468c80f092256eab5d0
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-07-02 16:15:39 +0200

    Swapped carets in review.js

--- js/review.js
+++ js/review.js
@@ -24,7 +24,7 @@ var carets = document.querySelectorAll('.up-down');
 
 carets.forEach(function(caret){
   caret.addEventListener("click", function(){
-     this.getElementsByTagName('span')[0].classList.toggle('fa-caret-down');
+     this.getElementsByTagName('span')[0].classList.toggle('fa-caret-right');
      this.getElementsByTagName('span')[0].classList.toggle('fa-caret-up');
   });
 })
