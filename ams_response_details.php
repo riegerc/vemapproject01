d89c59e8f0c6fa3edf752391589d2a4d01c402b0
Oliver Stingl <o.stingl@protonmail.com>
2019-07-02 12:16:19 +0200

    Broken page fix due to non-complete table and addition of col inc. taxes.

--- ams_response_details.php
+++ ams_response_details.php
@@ -4,52 +4,54 @@ require_once "include/constant.php";
 
 $pageRestricted = false; // defines if the page is restricted to logged-in Users only
 $userLevel = ""; // uses a PERM_ const now and hasPermission($userLevel) now if fails a 403 Error-Page is returned
-$title = ""; // defines the name of the current page, displayed in the title and as a header on the page
+$title = "Erstellte Ausschreibungen"; // defines the name of the current page, displayed in the title and as a header on the page
 
 include "include/init.php"; // includes base function like session handling
 include "include/page/top.php"; // top-part of html-template (stylesheets, navigation, ..)
-
-// TODO Abfrage das einem nur die Ausschreibungen angezeigt werden zu denen man eingeladen ist
 ?>
 
 <div class="container-fluid">
     <h1 class="h3 mb-4 text-gray-800"><?php echo $title ?></h1>
     <div class="content">
-
-        <h2>Erstellte Ausschreibungen</h2>
-
-
-
-        <?php
-        $supplierFID=$_GET["supplierFID"];
-        $tenderID=$_GET["tenderID"];
-        $amount=(int)$_GET["amount"];
-        $sql="SELECT  tendersresponse.price,tenderDetail.position FROM tendersresponse
-                INNER JOIN tenderDetail
-                ON tendersresponse.tenderDetailFID= tenderDetail.objectID
-                WHERE tendersresponse.supplierFID=:supplierFID AND tenderDetail.tendersFID=:tenderID";
-        $stmt = connectDB()->prepare($sql);
-        $stmt->bindParam(":supplierFID", $supplierFID);
-        $stmt->bindParam(":tenderID", $tenderID);
-        $stmt->execute();
-        echo "<table>
+        <table class="table table-striped">
+            <thead>
+            <tr>
                 <th>Position</th>
                 <th>Menge</th>
                 <th>Gesamtpreis</th>
-                <th>Preis per Stück</th>";
-        while ($row=$stmt->fetch()){
-            $total=(float)$row["price"] * $amount;
-            $proPice=(float)$row["price"];
-            echo "<tr><td>".$row["position"]."</td>";
-            echo "<td>$amount</td>";
-            echo "<td>". number_format($total,2,",",".") ." €" ."</td>";
-            echo "<td>". number_format($proPice,2,",",".") ." €" ."</td></tr>";
-        }
-        ?>
-    </div>
-    <div class="alert alert-primary" role="alert">
-        <p class="font-weight-light">Mehrwertsteuersatz = 20%</p>
+                <th>Preis inkl. Mwst.</th>
+                <th>Preis per Stück</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $supplierFID = $_GET["supplierFID"];
+            $tenderID = $_GET["tenderID"];
+            $amount = (int)$_GET["amount"];
+            $sql = "SELECT  tendersresponse.price,tenderDetail.position FROM tendersresponse
+                INNER JOIN tenderDetail
+                ON tendersresponse.tenderDetailFID= tenderDetail.objectID
+                WHERE tendersresponse.supplierFID=:supplierFID AND tenderDetail.tendersFID=:tenderID";
+            $stmt = connectDB()->prepare($sql);
+            $stmt->bindParam(":supplierFID", $supplierFID);
+            $stmt->bindParam(":tenderID", $tenderID);
+            $stmt->execute();
+
+            while ($row = $stmt->fetch()) {
+                $total = (float)$row["price"] * $amount;
+                $proPice = (float)$row["price"];
+                echo "<tr><td>" . $row["position"] . "</td>";
+                echo "<td>$amount</td>";
+                echo "<td>" . number_format($total, 2, ",", ".") . " €" . "</td>";
+                echo "<td>" . number_format($total * 1.2, 2, ",", ".") . " €" . "</td>";
+                echo "<td>" . number_format($proPice, 2, ",", ".") . " €" . "</td></tr>";
+            }
+            ?>
+            </tbody>
+        </table>
+        <div class="alert alert-primary" role="alert">
+            <p class="font-weight-light">Mehrwertsteuersatz = 20%</p>
+        </div>
     </div>
 </div>
-
 <?php include "include/page/bottom.php"; // bottom-part of html-template (footer, scripts, .. ) ?>
