3337412407e899db584faf7daf229960cdea8e2e
themrvideouser <frederik.gschaider@gmail.com>
2019-07-03 10:13:04 +0200

    linked sitemap

--- sitemap.php
+++ sitemap.php
@@ -17,30 +17,30 @@ include "include/page/top.php"; // top-part of html-template (stylesheets, navig
     <div class="content">
         <!-- Content -->
         <ul>
-            <li><a href="#">Home</a> </li>
+            <li><a href="index.php">Home</a> </li>
             <li><a href="#">Ausschreibungen</a>
                 <ul>
-                    <li><a href="#">Ihre Ausschreibungen</a></li>
-                    <li><a href="#">Lieferant anlegen</a></li>
-                    <li><a href="#">Ausschreibung erstellen</a></li>
+                    <li><a href="overview_tenders.php">Ihre Ausschreibungen</a></li>
+                    <li><a href="create_supplier.php">Lieferant anlegen</a></li>
+                    <li><a href="create_tender.php">Ausschreibung erstellen</a></li>
                 </ul>
             </li>
             <li><a href="#">Webshop</a>
                 <ul>
-                    <li><a href="#">Kunde</a></li>
-                    <li><a href="#">Lieferant</a></li>
+                    <li><a href="webshop_kunde.php">Kunde</a></li>
+                    <li><a href="webshop_lieferant.php">Lieferant</a></li>
                 </ul></li>
             <li><a href="#">Bewertungen</a>
                 <ul>
-                    <li><a href="#">Übersicht</a></li>
-                    <li><a href="#">Bewertung verwalten</a></li>
-                    <li><a href="#">Lieferant bewerten</a></li>
-                    <li><a href="#">Auswertung</a></li>
+                    <li><a href="review_uebersichts_tabelle.php">Übersicht</a></li>
+                    <li><a href="kriterien.php">Bewertung verwalten</a></li>
+                    <li><a href="lieferant.php?what=1">Lieferant bewerten</a></li>
+                    <li><a href="lieferant.php?what=2">Auswertung</a></li>
                 </ul></li>
             <li><a href="#">User</a>
                 <ul>
-                    <li><a href="#">Übersicht</a></li>
-                    <li><a href="#">Erstellen</a></li>
+                    <li><a href="user.php">Übersicht</a></li>
+                    <li><a href="create_user.php">Erstellen</a></li>
                 </ul></li>
         </ul>
     </div>
