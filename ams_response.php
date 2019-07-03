a483c5a18857d68d295f457270d88f1376a8a6ba
hani-mansour <hani.mansour.1990@gmail.com>
2019-07-03 09:31:47 +0200

    fixed ams_response_details and ams_response_details

--- ams_response.php
+++ ams_response.php
@@ -10,31 +10,24 @@ include "include/init.php"; // includes base function like session handling
 include "include/page/top.php"; // top-part of html-template (stylesheets, navigation, ..)
 
 
-$sql="SELECT tenders.objectID, tenders.tender,tenderDetail.amount AS amount FROM tenders 
-        INNER JOIN tenderDetail
-        ON tenders.objectID = tenderDetail.tendersFID ";
-$st = connectDB()->query($sql);
-#$x=$st->fetch();
-
-
 // TODO Abfrage das einem nur die Ausschreibungen angezeigt werden zu denen man eingeladen ist
 ?>
 
-<div class="container-fluid">
-    <h1 class="h3 mb-4 text-gray-800"><?php echo $title ?></h1>
-    <div class="content">
+    <div class="container-fluid">
+        <h1 class="h3 mb-4 text-gray-800"><?php echo $title ?></h1>
+        <div class="content">
 
-        <h2>Erstellte Ausschreibungen</h2>
+            <h2>Erstellte Ausschreibungen</h2>
 
+            <?php
+            $sql="SELECT objectID, tender FROM tenders ";
+            $st = connectDB()->query($sql);
+            echo "<table>";
 
-
-        <?php
-        echo "<table>";
-
-        while($id=$st->fetch()){
-            $tenderID=(int)$id["objectID"];
-            $ten=$id["tender"];
-            $sql="SELECT tenders.objectID AS tenderID,tendersresponse.supplierFID AS supplierFID,tenders.tender,user.branchName,SUM(tendersresponse.price) As TotalPrice,supplierselect.userFID FROM tendersresponse
+            while($id=$st->fetch()){
+                $tenderID=(int)$id["objectID"];
+                $ten=$id["tender"];
+                $sql="SELECT tenders.objectID AS tenderID,tendersresponse.supplierFID AS supplierFID,tenders.tender,user.branchName,SUM(tendersresponse.price) As TotalPrice,supplierselect.userFID FROM tendersresponse
                 INNER JOIN user
                 ON tendersresponse.supplierFID = user.objectID
                 INNER JOIN supplierselect
@@ -43,37 +36,37 @@ $st = connectDB()->query($sql);
                 ON supplierselect.tenderFID = tenders.objectID
                 WHERE tenders.objectID=:tenderID
                 GROUP BY tendersresponse.supplierFID,tenders.tender";
-            $stmt = connectDB()->prepare($sql);
-            $stmt->bindParam(":tenderID", $tenderID);
-            $stmt->execute();
-            $row = $stmt->fetch();
-            if(!$row) continue;
-            #var_dump($row);
-            echo "<tr><th colspan='3'><h3>$ten</h3></th></tr>";
-            echo "<th>Firma</th>
-                <th>Gesamtpreis</th>
-                <th>Details</th>";
+                $stmt = connectDB()->prepare($sql);
+                $stmt->bindParam(":tenderID", $tenderID);
+                $stmt->execute();
+                $row = $stmt->fetch();
+                if(!$row) continue;
+                #var_dump($row);
+                echo "<tr><th colspan='3'>$ten</th></tr>";
+                echo "<th>Firma</th>
+                <th>Preis</th>
+                <th>Details</th><tr>";
 
-            do{
-                #echo $row["tender"];
-                $total=(float)$row["TotalPrice"] * $id["amount"];
-                echo "
-                <tr>
-                <td>$row[branchName]</td>" .
-                "<td>" . number_format($total,2,",",".") ." €" ."</td>".
-                "<td><a href='ams_response_details.php?supplierFID=$row[supplierFID]&tenderID=$row[tenderID]&amount=$id[amount]' class='btn btn-info'>Details</a></td>
-                </tr>";
-            }while ($row = $stmt->fetch());
-            echo "<tr><td>&nbsp;</td></tr>";
-        }
-        echo "</table>";
+                do{
+                    #echo $row["tender"];
+                    echo "";
+                    echo "
+                        <td>$row[branchName]</td>" .
+                        "<td>" . number_format($row["TotalPrice"],2,",",".") ." €" ."</td>".
+                        "<td><a href='ams_response_details.php?supplierFID=$row[supplierFID]&tenderID=$row[tenderID]' class='btn btn-info'>Details</a></td></tr>";
+                }while ($row = $stmt->fetch());
 
-        ?>
+                echo "<tr><td>&nbsp;</td></tr>";
+            }
+            echo "</table>";
+
+            ?>
+
+        </div>
+        <div class="alert alert-primary" role="alert">
+            <p class="font-weight-light">Mehrwertsteuersatz = 20%</p>
+        </div>
     </div>
-    <div class="alert alert-primary" role="alert">
-        <p class="font-weight-light">Mehrwertsteuersatz = 20%</p>
-    </div>
-</div>
 
 
 <?php include "include/page/bottom.php"; // bottom-part of html-template (footer, scripts, .. ) ?>
