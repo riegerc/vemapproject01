3030aceaa9877764f70de0367d04d318ff01ebae
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-07-03 08:44:48 +0200

    Removed textarea completely

--- fragebogen.php
+++ fragebogen.php
@@ -12,7 +12,6 @@ include "include/page/top.php"; // top-part of html-template (stylesheets, navig
 include "include/helper.inc.php"; // top-part of html-template (stylesheets, navigation, ..)
 include "classes/types/fragebogen.inc.php"; // top-part of html-template (stylesheets, navigation, ..)
 require_once("classes/repository.inc.php");
-$isFormSubmitted=false;
 $userId=$_SESSION[USER_ID];
 $rep=new Repository();
 $fragen=$rep->readFragebogen();
@@ -20,8 +19,7 @@ $lieferantid=0;
 if(isset($_GET["lieferantid"])){
 	$lieferantid=(int)Helper::sanitize($_GET["lieferantid"]);	
 }
-if(isset($_POST["senden"])){
-	$isFormSubmitted=true;
+if(isset($_POST["senden"])){	
 	echo "<pre>";
 	//print_r($_POST);
 	echo "</pre>";
@@ -52,61 +50,29 @@ if(isset($_POST["senden"])){
 <div class="container-fluid">
     <h1 class="h3 mb-4 text-gray-800"><?php echo $title ?></h1>
     <div class="content">
-	<h1 style="color:red">Under construction</h1>
+		<h1 style="color:red">Under construction</h1>
 		<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
 			<label class="mth-label">Just 4 Test - Monat: </label> <input class="mth-inp" type="number" min="1" max="6" name="month">
 			<input type="hidden" value="<?php echo $lieferantid; ?>" name="lieferantid">
-				<?php
-				$shouldComment=false;
-				
-					foreach($fragen as $frage) {
-
-						echo "<h2>" . $frage->getName() . "</h2>\n";
-						echo "<ul class='list-group list-group-flush' id='slds" . $frage->getId() . "'>\n";
-						foreach($frage->getKriterien() as $kriterium) {
-							echo "<li class='list-group-item'>";
-							
-							$maxInputRange=round($kriterium->getPrzt(),0);
-							
-							echo "<div class='form-group'>\n";
-						    echo "<label for='sld" . $kriterium->getId() . "'>" . $kriterium->getName() . "</label>\n";
-							echo "<input type='range' class='form-control-range custom-range' id='sld" . $kriterium->getId() . "' min='0' max='$maxInputRange' value='0' step='0.001' name='sld" . $kriterium->getId() . "' onchange='setLabelText(" . $kriterium->getId() . "," . $kriterium->getFkKriterium() . ")'>\n";
-							
-							echo "<span class='float-left'>0</span>\n";
-							echo "<span class='float-right'>" . $maxInputRange . "</span>\n";
-							
-							echo "</div>";
-							
-							echo "</li>";
-						}
-						echo "</ul>\n";
-				
-						echo "<div id='lbl" . $kriterium->getFkKriterium() . "'>0</div>\n";
-						
-						$collapse = "collapse";
-
-						$invalid="";
-						/*
-						if ($isFormSubmitted) {
-							
-							if (($key = array_search($kriterium->getFkKriterium(), $toComment)) !== false) {
-								$shouldComment=true;
-							}
-							
-							if ($shouldComment==true) {
-								$collapse = "";
-							    $invalid = "is-invalid";
-							} else {
-								$collapse = "collapse";
-								$invalid = "";
-							}							
-						}
-						*/
-						echo "<p class='up-down' data-toggle='collapse' data-target='#target" . $kriterium->getFkKriterium() . "' id='chk" . $kriterium->getFkKriterium() . "'><span class='fa fa-caret-right'></span>\n";
-						echo "<label class='form-check-label' for='chk" . $kriterium->getFkKriterium() . "'>Kommentar</label></p>\n";
-						//echo "<textarea class='form-control $collapse $invalid' name='txt" . $kriterium->getFkKriterium() . "' id='target" . $kriterium->getFkKriterium() . "'></textarea>\n";
-					}
-					?>			
+				<?php		
+				foreach($fragen as $frage) {
+				echo "<h2>" . $frage->getName() . "</h2>\n";
+				echo "<ul class='list-group list-group-flush' id='slds" . $frage->getId() . "'>\n";
+				foreach($frage->getKriterien() as $kriterium){
+					$maxInputRange=round($kriterium->getPrzt(),0);
+					echo "<li class='list-group-item\n'>";		
+					echo "<div class='form-group'>\n";
+					echo "<label for='sld" . $kriterium->getId() . "'>" . $kriterium->getName() . "</label>\n";
+					echo "<input type='range' class='form-control-range custom-range' id='sld" . $kriterium->getId() . "' min='0' max='$maxInputRange' value='0' step='0.001' name='sld" . $kriterium->getId() . "' onchange='setLabelText(" . $kriterium->getId() . "," . $kriterium->getFkKriterium() . ")'>\n";		
+					echo "<span class='float-left'>0</span>\n";
+					echo "<span class='float-right'>" . $maxInputRange . "</span>\n";
+					echo "</div>\n";	
+					echo "</li>\n";
+				}
+				echo "</ul>\n";
+				echo "<div id='lbl" . $kriterium->getFkKriterium() . "'>0</div>\n";					
+				}
+				?>			
 			<div class="form-row">
 				<div class="col-md-2">
 					<button type="submit" name="senden" class="btn btn-primary form-button">Senden</button>
