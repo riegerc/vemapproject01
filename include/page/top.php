fabfb255ceeaa850664d5ff36c77e46ec85ceeed
themrvideouser <frederik.gschaider@gmail.com>
2019-07-05 08:58:23 +0200

    username disappearing

--- include/page/top.php
+++ include/page/top.php
@@ -27,7 +27,7 @@
                             <li class='nav-item dropdown no-arrow'>
                                 <a class='nav-link dropdown-toggle' href='#' id='userDropdown' role='button'
                                    data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                                    <span class='mr-2 d-none d-lg-inline text-gray-600 small'>" .
+                                    <span class='mr-2 d-inline d-lg-inline text-gray-600 small'>" .
                                         $_SESSION[USER_NAME]
                         . "</span>
                                 </a>
